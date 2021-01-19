8988a7edd982bbe22538dc1575e90770c1b06f96
isakari <isakari@nuem.nagoya-u.ac.jp>
2021-01-19 18:36:55 +0900

    GLQ: done

--- a.sh
+++ a.sh
@@ -1,6 +1,9 @@
 cd 1d
 pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
+cd ../GLQ
+pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
+
 cd ../Einstein
 pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
