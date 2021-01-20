3790c66abf79d7b02e8e79f50fedc088182ce626
isakari <isakari@nuem.nagoya-u.ac.jp>
2021-01-20 10:50:02 +0900

    modified a.sh

--- a.sh
+++ a.sh
@@ -1,14 +1,14 @@
 cd 1d
-pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
+pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango --metadata pagetitle="1d FEM" README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
 cd ../GLQ
-pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
+pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango --metadata pagetitle="Gauss-Legendre quadrature" README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
 cd ../Einstein
-pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
+pandoc -s --filter pandoc-crossref -M "crossrefYaml=../crossref_config.yaml" --template=../github.html --mathjax --highlight-style=tango --metadata pagetitle="Summation convention" README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
 cd ../
-pandoc -s --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" --template=github.html --mathjax --highlight-style=tango README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
+pandoc -s --filter pandoc-crossref -M "crossrefYaml=crossref_config.yaml" --template=github.html --mathjax --highlight-style=tango --metadata pagetitle="Computational mechanics" README.md -o index.html && sed 's/README.md/index.html/' index.html > tmp && mv tmp index.html
 
 
 
